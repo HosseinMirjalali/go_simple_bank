4811be1b9679455fd306ee7cfb771966690bf880
Hossein <mirjalali.hosein@gmail.com>
2022-11-27 15:34:27 +0330

    end of lecture 5, just added entry

---
+++ db/migration/000001_init_schema.down.sql
@@ -0,0 +1,6 @@
+--DROP TALBE IF EXISTS entries CASCADE;
+--DROP TALBE IF EXISTS transfers CASCADE;
+--DROP TALBE IF EXISTS accounts CASCADE;
+DROP TABLE IF EXISTS "public"."entries";
+DROP TABLE IF EXISTS "public"."transfers";
+DROP TABLE IF EXISTS "public"."accounts";
